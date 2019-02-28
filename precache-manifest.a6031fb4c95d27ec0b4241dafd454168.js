029f531136fed817b4aedd22805de02b6f1c2561
thilinal47 <thilinal47@gmail.com>
2019-02-28 18:38:43 +0530

    Updates

---
+++ precache-manifest.a6031fb4c95d27ec0b4241dafd454168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c4b54bfd49c02f9eaead",
+    "url": "/site/static/js/runtime~main.c4b54bfd.js"
+  },
+  {
+    "revision": "029e6f0a2e569686620d",
+    "url": "/site/static/js/main.029e6f0a.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/site/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "029e6f0a2e569686620d",
+    "url": "/site/static/css/main.63934498.chunk.css"
+  },
+  {
+    "revision": "09e245af5209c9bdb47a3fbb941d4d60",
+    "url": "/site/index.html"
+  }
+];
